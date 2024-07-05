7ff2ffe1d6e73194bd1159ef2adb748f63cc3171
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-07-05 14:49:39 +0100

    Refactored to make search api componet more generic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent } from "react";
-import SearchApiBox from "./components/SearchApiBox";
+import SearchApiBox, { RenderComponent } from "./components/SearchApiBox";
 import axios, { AxiosResponse } from "axios";
-import { ProductAPIResponse } from "./types/products";
+import { Product, ProductAPIResponse } from "./types/products";
+import { ComboboxOption } from "@headlessui/react";
+import { MdCheckCircleOutline } from "react-icons/md";
 
 interface AppProps {}
 
@@ -14,9 +16,38 @@ const fetchSearchProducts = async (
 const App: FunctionComponent<AppProps> = () => {
   return (
     <section className="container mx-auto p-4">
-      <SearchApiBox fetchFn={fetchSearchProducts} />
+      <SearchApiBox
+        fetchFn={fetchSearchProducts}
+        renderComponent={RenderProducts}
+        displayValue={(product: Product) => product?.title || ""}
+      />
     </section>
   );
 };
 
 export default App;
+
+const RenderProducts: RenderComponent<ProductAPIResponse> = (data) => {
+  return (
+    <>
+      {data.products.length === 0 ? (
+        <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
+          Nothing found.
+        </div>
+      ) : (
+        data?.products.map((product: Product) => (
+          <ComboboxOption
+            key={product.id}
+            className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-indigo-600 data-[focus]:text-white"
+            value={product}
+          >
+            <MdCheckCircleOutline className="invisible size-4 fill-indigo-600 group-data-[focus]:fill-white group-data-[selected]:visible" />
+            <div className="text-sm/6 group-data-[selected]:font-bold">
+              {product.title}
+            </div>
+          </ComboboxOption>
+        ))
+      )}
+    </>
+  );
+};
--- src/components/SearchApiBox.tsx
+++ src/components/SearchApiBox.tsx
@@ -2,23 +2,14 @@ import {
   Combobox,
   ComboboxButton,
   ComboboxInput,
-  ComboboxOption,
   ComboboxOptions,
   Transition,
 } from "@headlessui/react";
 import { useQuery } from "@tanstack/react-query";
 import debounce from "lodash.debounce";
-import {
-  Fragment,
-  FunctionComponent,
-  useCallback,
-  useMemo,
-  useState,
-} from "react";
-import { BiChevronDown } from "react-icons/bi";
-import { MdCheckCircleOutline } from "react-icons/md";
-import { Product, ProductAPIResponse } from "../types/products";
+import { Fragment, useCallback, useMemo, useState } from "react";
 import { AxiosError, AxiosResponse } from "axios";
+import { HiOutlineChevronUpDown } from "react-icons/hi2";
 
 // Debounce function using react-query to fetch data
 const useDebouncedSearch = <T, E>(
@@ -31,27 +22,34 @@ const useDebouncedSearch = <T, E>(
     staleTime: 5 * 60 * 1000,
   });
 };
+export type RenderComponent<T> = (data: T) => JSX.Element;
 
-interface SearchApiBoxProps {
+interface SearchApiBoxProps<T> {
   fetchFn: (query: string) => Promise<any>;
+  renderComponent: RenderComponent<T>;
+  displayValue: (data: any) => string;
 }
 
-const SearchApiBox: FunctionComponent<SearchApiBoxProps> = ({ fetchFn }) => {
+const SearchApiBox = <T,>({
+  fetchFn,
+  renderComponent,
+  displayValue,
+}: SearchApiBoxProps<T>) => {
   const [debouncedQuery, setDebouncedQuery] = useState("");
-  const [selected, setSelected] = useState<Product | null>(null);
+  const [selected, setSelected] = useState<T | null>(null);
 
   // Fetch data using useDebouncedSearch hook on component mount/render using search query and props fetch function
-  const { data } = useDebouncedSearch<
-    AxiosResponse<ProductAPIResponse, any>,
-    AxiosError
-  >(debouncedQuery, fetchFn);
+  const { data } = useDebouncedSearch<AxiosResponse, AxiosError>(
+    debouncedQuery,
+    fetchFn
+  );
 
   // Memoize the debounced search function and delay setterFunc by 500ms
   const debouncedSearch = useMemo(
     () =>
       debounce((query: string) => {
         setDebouncedQuery(query);
-      }, 500),
+      }, 250),
     []
   );
 
@@ -65,68 +63,31 @@ const SearchApiBox: FunctionComponent<SearchApiBoxProps> = ({ fetchFn }) => {
   );
 
   return (
-    <div className="fixed top-16 w-72">
-      <Combobox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
-          <div className="relative w-full cursor-default rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-            <ComboboxInput
-              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(product: Product) => product?.title || ""}
-              onChange={handleChange}
-            />
-            <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <BiChevronDown
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </ComboboxButton>
-          </div>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setDebouncedQuery("")}
-          >
-            <ComboboxOptions className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {data?.data.products.length === 0 ? (
-                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-                  Nothing found.
-                </div>
-              ) : (
-                data?.data?.products.map((product: Product) => (
-                  <ComboboxOption
-                    key={product.id}
-                    className=" relative cursor-default select-none py-2 pl-10 pr-4 text-gray-900 data-[focus]:bg-teal-600 data-[focus]:text-white"
-                    value={product}
-                  >
-                    {({ selected }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
-                          {product.title}
-                        </span>
-                        {selected ? (
-                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-teal-600 data-[focus]:text-white">
-                            <MdCheckCircleOutline
-                              className="h-5 w-5"
-                              aria-hidden="true"
-                            />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </ComboboxOption>
-                ))
-              )}
-            </ComboboxOptions>
-          </Transition>
+    <Combobox value={selected} onChange={setSelected}>
+      <div className="relative mt-1">
+        <div className="relative mx-auto bg-white rounded-full">
+          <ComboboxInput
+            className="w-full border-none py-2 pl-3 pr-10 text-sm  shadow-md leading-5 text-gray-900 rounded-full focus:ring-0 outline-indigo-600 caret-indigo-600"
+            displayValue={(item) => displayValue(item)}
+            onChange={handleChange}
+          />
+          <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2 hover:text-indigo-600 text-gray-400 focus:text-indigo-600">
+            <HiOutlineChevronUpDown className="h-5 w-5" aria-hidden="true" />
+          </ComboboxButton>
         </div>
-      </Combobox>
-    </div>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setDebouncedQuery("")}
+        >
+          <ComboboxOptions className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+            {data?.data && renderComponent(data.data)}
+          </ComboboxOptions>
+        </Transition>
+      </div>
+    </Combobox>
   );
 };
 
